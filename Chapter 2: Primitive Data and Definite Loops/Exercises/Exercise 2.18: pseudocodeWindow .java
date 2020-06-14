06b6d80e4f9248d825425b87dcca05e80947ff96
Bauaser <62292070+Bauaser@users.noreply.github.com>
2020-06-14 21:22:03 +0200

    Create Exercise 2.18: pseudocodeWindow .java

---
+++ Chapter 2: Primitive Data and Definite Loops/Exercises/Exercise 2.18: pseudocodeWindow .java
@@ -0,0 +1,23 @@
+//Write a pseudocode algorithm that will produce the following figure as output:
+
+//    +===+===+
+//    |   |   |
+//    |   |   |
+//    |   |   |
+//    +===+===+
+//    |   |   |
+//    |   |   |
+//    |   |   |
+//    +===+===+
+
+//(Since this is just pseudo-code, Practice-It is not able to verify that your solution is "correct". 
+//But you can submit an answer anyway to show that you completed this exercise. Any non-trivial answer will be accepted.)
+
+pseudocode:
+System.out.println(+===+===+);
+for (int i=1; i<=3; i++){
+System.out.println("|   |   |");}
+System.out.println(+===+===+);
+for (int i=1; i<=3; i++){
+System.out.println("|   |   |");}
+System.out.println(+===+===+);
