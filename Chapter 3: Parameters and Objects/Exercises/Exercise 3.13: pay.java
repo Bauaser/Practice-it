e882431d62d09f80337db4645cb3276555083e7d
Bauaser <62292070+Bauaser@users.noreply.github.com>
2020-06-17 15:35:36 +0200

    Create Exercise 3.13: pay.java

---
+++ Chapter 3: Parameters and Objects/Exercises/Exercise 3.13: pay.java
@@ -0,0 +1,15 @@
+//Write a method named pay that accepts a real number for a TA's salary and an integer for the number of hours the TA worked this week, 
+//and returns how much money to pay the TA. For example, the call pay(5.50, 6) should return 33.0.
+
+//The TA should receive "overtime" pay of 1 ½ normal salary for any hours above 8.
+//For example, the call pay(4.00, 11) should return (4.00 * 8) + (6.00 * 3) or 50.0.
+
+public static double pay (double x, int y){
+if (y<=8){
+double pay = x*y;
+return pay;
+} else {
+double pay = ((x*8)+((y-8)*x*1.5));
+return pay;
+}
+    }
