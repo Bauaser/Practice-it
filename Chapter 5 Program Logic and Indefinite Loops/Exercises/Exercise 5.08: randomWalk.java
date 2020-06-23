e1ddd98382ed5414f5d6d6539652121e628d6c3b
Bauaser <62292070+Bauaser@users.noreply.github.com>
2020-06-23 15:53:48 +0200

    Create Exercise 5.08: randomWalk.java

---
+++ Chapter 5 Program Logic and Indefinite Loops/Exercises/Exercise 5.08: randomWalk.java
@@ -0,0 +1,16 @@
+//(This problem is a modified version of Chapter 5's randomWalk exercise from the textbook.)
+
+//Write a method named randomWalk that performs a random one-dimensional walk, reporting each position reached and the maximum position reached during the walk. 
+//The random walk should begin at position 0. On each step, you should either increase or decrease the position by 1 (with equal probability). 
+//The walk stops when 3 or -3 is hit. The output should look like this:
+
+//position = 0
+//position = 1
+//position = 0
+//position = -1
+//position = -2
+//position = -1
+//position = -2
+//position = -3
+//max position = 1
+
