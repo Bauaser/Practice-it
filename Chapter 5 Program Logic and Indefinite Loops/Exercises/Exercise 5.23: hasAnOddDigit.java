212a5705740ca6412f1f3922ba07351f01864d12
Bauaser <62292070+Bauaser@users.noreply.github.com>
2020-06-24 22:54:32 +0200

    Create Exercise 5.23: hasAnOddDigit.java

---
+++ Chapter 5 Program Logic and Indefinite Loops/Exercises/Exercise 5.23: hasAnOddDigit.java
@@ -0,0 +1,23 @@
+//Write a method named hasAnOddDigit that returns whether any digit of an integer is odd. 
+//Your method should return true if the number has at least one odd digit and false if none of its digits are odd. 
+//0, 2, 4, 6, and 8 are even digits, and 1, 3, 5, 7, 9 are odd digits.
+
+//For example, here are some calls to your method and their expected results:
+//Call 	                  Value Returned
+//hasAnOddDigit(4822116) 	    true
+//hasAnOddDigit(2448) 	      false
+//hasAnOddDigit(-7004) 	      true
+
+//You should not use a String to solve this problem
+
+public static boolean hasAnOddDigit(int x){
+boolean odd = false;
+    x=Math.abs(x);
+    while (x>0) {
+    if (x%2==1){
+    odd =true;
+    }
+        x=x/10;
+    }
+    return odd;
+}
